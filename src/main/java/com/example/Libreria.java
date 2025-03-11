3171d5d3db995fc9c5e1cfb410e018f7854836fb
macchina_kali <iacopoliberolavoro@gmail.com>
2025-03-11 10:00:23 +0100

    Implement library management system with classes for Libreria and Libro, allowing user input for book details and displaying the book list.

--- src/main/java/com/example/Libreria.java
+++ src/main/java/com/example/Libreria.java
@@ -1,6 +1,37 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Libreria {
     
     private String nome;
+    private ArrayList<Libro>libri;
+
+    public Libreria(String nome) {
+        this.nome = nome;
+        this.libri = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Libro> getLibri() {
+        return libri;
+    }
+
+    @Override
+    public String toString() {
+        return "Libreria{" + "nome=" + nome + ", libri=" + libri + '}';
+    }
+
+    public void aggiungiLibro(Libro l)
+    {
+        libri.add(l);
+    }
+    
 }
